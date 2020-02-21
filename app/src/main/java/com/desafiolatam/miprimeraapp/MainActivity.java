73fed127b0b705644c9534beba10e97074cc782e
camilo <kamilo_m_10@hotmail.com>
2020-02-21 16:24:07 -0300

    commit de todo

--- app/src/main/java/com/desafiolatam/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/desafiolatam/miprimeraapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.desafiolatam.miprimeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView nuevaimagen = findViewById(R.id.imagen);
+
+
+        Picasso.get().load("https://guide.fire-emblem-heroes.com/wp-content/uploads/myrrh_great_dragon.png").into(nuevaimagen);
+
+        Button mybuttom = findViewById(R.id.mybuttom);
+        mybuttom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "acabas de ingresar a jumanji", Toast.LENGTH_SHORT).show();
+
+                intentExample();
+            }
+        });
+    }
+
+    private void intentExample(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
