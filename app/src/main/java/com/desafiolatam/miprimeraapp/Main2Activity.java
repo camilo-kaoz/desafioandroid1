7af5363d282107c048d28748c2a0e3237d961467
camilo <kamilo_m_10@hotmail.com>
2020-02-26 12:49:38 -0300

    termino desafio 2

--- app/src/main/java/com/desafiolatam/miprimeraapp/Main2Activity.java
+++ app/src/main/java/com/desafiolatam/miprimeraapp/Main2Activity.java
@@ -1,32 +1,45 @@
 package com.desafiolatam.miprimeraapp;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageView nuevaimagen = findViewById(R.id.imagen);
+
+
+        Picasso.get()
+                .load("https://images.unsplash.com/photo-1499618838676-f1a9bc076843?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")
+                .resize(1200, 600)
+                .into(nuevaimagen);
+
+        Button mybuttom = findViewById(R.id.mybuttom);
+        mybuttom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(Main2Activity.this, "button press", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "acabas de ingresar a jumanji", Toast.LENGTH_SHORT).show();
+
+                intentExample();
             }
         });
     }
 
+    private void intentExample(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/desafiolatam/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/desafiolatam/miprimeraapp/MainActivity.java
@@ -1,42 +1,34 @@
 package com.desafiolatam.miprimeraapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView nuevaimagen = findViewById(R.id.imagen);
+        ImageView ojos = findViewById(R.id.imagen);
 
 
-        Picasso.get().load("https://guide.fire-emblem-heroes.com/wp-content/uploads/myrrh_great_dragon.png").into(nuevaimagen);
+        Picasso.get()
+                .load("https://images.unsplash.com/photo-1499618838676-f1a9bc076843?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")
+                .resize(1200, 600)
+                .into(ojos);
 
-        Button mybuttom = findViewById(R.id.mybuttom);
-        mybuttom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+      /*  Toolbar toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);*/
 
-                Toast.makeText(MainActivity.this, "acabas de ingresar a jumanji", Toast.LENGTH_SHORT).show();
 
-                intentExample();
-            }
-        });
-    }
-
-    private void intentExample(){
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
-    }
-}
+    }}
